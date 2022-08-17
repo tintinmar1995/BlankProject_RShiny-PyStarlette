1a52ae470e6156cc91fab3dc17684f326e6999da
Martin M <martin.masson@datastorm.fr>
2022-08-17 17:57:19 +0200

    FEAT: Print http response in verbatim UI

--- frontend/src/inst/shiny/server.R
+++ frontend/src/inst/shiny/server.R
@@ -1,7 +1,13 @@
+require(httr)
+
 shinyServer(function(input, output, session) {
 
   output$content = renderText({
-    return(tryCatch(expr = {"Communication réussie"}, error=function(e){
+    return(tryCatch(expr = {
+      backend = Sys.getenv('URL_BACKEND')
+      response = httr::GET(backend)
+      return(httr::content(response))
+      }, error=function(e){
      "Communication en échec" 
     }))
   })
